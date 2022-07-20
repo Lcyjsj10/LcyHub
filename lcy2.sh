8f5f94822f79e13da84c4ae3ccb2096934d89a90
unknown <2948713302@qq.com>
2022-07-21 01:28:55 +0800

    Thu Jul 21 01:28:55     2022

---
+++ lcy2.sh
@@ -0,0 +1,5 @@
+git init
+git remote add origin https://github.com/Lcyjsj10/LcyHub.git
+add
+commit
+push origin master
---
+++ lcy3.sh
@@ -0,0 +1,10 @@
+git init
+git remote add origin https://github.com/Lcyjsj10/LcyHub.git
+git fetch origin
+git merge origin/master
+ 
+然后对项目进行一些修改
+ 
+git add .
+commit  
+git push origin master
